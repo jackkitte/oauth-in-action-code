331dd5495691a9a0a020df824212db46b476d54e
jackkitte <jackkitte735@gmail.com>
2019-07-13 18:49:34 +0900

    client.jsにてリダイレクト処理の追加とコールバックした時のトークン表示処理

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -27,8 +27,8 @@ var authServer = {
  * Add the client information in here
  */
 var client = {
-	"client_id": "",
-	"client_secret": "",
+	"client_id": "oauth-client-1",
+	"client_secret": "oauth-client-secret-1",
 	"redirect_uris": ["http://localhost:9000/callback"]
 };
 
@@ -45,17 +45,55 @@ app.get('/', function (req, res) {
 
 app.get('/authorize', function(req, res){
 
-	/*
-	 * Send the user to the authorization server
-	 */
+	access_token = null;
+
+	state = randomstring.generate();
+	
+	var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
+		response_type: 'code',
+		client_id: client.client_id,
+		redirect_uri: client.redirect_uris[0],
+		state: state
+	});
+	
+	console.log("redirect", authorizeUrl);
+	res.redirect(authorizeUrl);
 	
 });
 
 app.get('/callback', function(req, res){
 
-	/*
-	 * Parse the response from the authorization server and get a token
-	 */
+	if (req.query.state != state) {
+		console.log('State DOES NOT MATCH: expected %s got %s', state, req.query.state);
+		res.render('error', {error: 'State value did not match'});
+		return;
+	}
+
+	var code = req.query.code;
+
+	var form_data = qs.stringify({
+		grant_type: 'authorization_code',
+		code: code,
+		redirect_uri: client.redirect_uris[0]
+	});
+	var headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
+	};
+
+	var tokRes = request('POST', authServer.tokenEndpoint, {	
+			body: form_data,
+			headers: headers
+	});
+
+	console.log('Requesting access token for code %s',code);
+	
+	var body = JSON.parse(tokRes.getBody());
+
+	access_token = body.access_token;
+	console.log('Got access token: %s', access_token);
+	
+	res.render('index', {access_token: access_token, scope: scope});
 	
 });
 
